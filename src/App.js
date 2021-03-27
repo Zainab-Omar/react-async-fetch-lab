69d510a5d067376927a95cd8f3731e905930c76a
Zainab Omar <zainab.alshaikhli01@gmail.com>
2021-03-27 15:05:03 -0500

    commit tree 9e75448371d6a4010387775bcd3613438068cc35

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+import React from 'react'
+
+export default class App extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            names: []
+        }
+    }
+    componentDidMount(){
+        fetch('http://api.open-notify.org/astros.json')
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                names: json.people
+            })
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h1>People on Space</h1>
+                <ul>{this.state.names.map(name => <li>{name.name}</li>)}</ul>
+                
+            </div>
+        )
+    }
+}
